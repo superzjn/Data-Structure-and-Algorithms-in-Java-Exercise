925052fbabaeea63a3265347a22b4e9db3decbe3
Nan Zhang <nan.zhang@morningstar.com>
2019-05-14 14:58:44 -0500

    Chapter3 projects - project 3.3 in progress

--- Chap03/BubbleSort/bubbleSort.java
+++ Chap03/BubbleSort/bubbleSort.java
@@ -2,73 +2,116 @@
 // demonstrates bubble sort
 // to run this program: C>java BubbleSortApp
 ////////////////////////////////////////////////////////////////
-class ArrayBub
-   {
-   private long[] a;                 // ref to array a
-   private int nElems;               // number of data items
-//--------------------------------------------------------------
-   public ArrayBub(int max)          // constructor
-      {
-      a = new long[max];                 // create the array
-      nElems = 0;                        // no items yet
-      }
-//--------------------------------------------------------------
-   public void insert(long value)    // put element into array
-      {
-      a[nElems] = value;             // insert it
-      nElems++;                      // increment size
-      }
-//--------------------------------------------------------------
-   public void display()             // displays array contents
-      {
-      for(int j=0; j<nElems; j++)       // for each element,
-         System.out.print(a[j] + " ");  // display it
-      System.out.println("");
-      }
-//--------------------------------------------------------------
-   public void bubbleSort()
-      {
-      int out, in;
+class ArrayBub {
+    private long[] a;                 // ref to array a
+    private int nElems;               // number of data items
 
-      for(out=nElems-1; out>1; out--)   // outer loop (backward)
-         for(in=0; in<out; in++)        // inner loop (forward)
-            if( a[in] > a[in+1] )       // out of order?
-               swap(in, in+1);          // swap them
-      }  // end bubbleSort()
+    //--------------------------------------------------------------
+    public ArrayBub(int max)          // constructor
+    {
+        a = new long[max];                 // create the array
+        nElems = 0;                        // no items yet
+    }
+
+    //--------------------------------------------------------------
+    public void insert(long value)    // put element into array
+    {
+        a[nElems] = value;             // insert it
+        nElems++;                      // increment size
+    }
+
+    //--------------------------------------------------------------
+    public void display()             // displays array contents
+    {
+        for (int j = 0; j < nElems; j++)       // for each element,
+            System.out.print(a[j] + " ");  // display it
+        System.out.println("");
+    }
+
+    //--------------------------------------------------------------
+//    public void bubbleSort() {
+//        int out, in;
+//
+//        // out need to be >=1 otherwise the 1st and 2nd elements may not be sorted if number of elements is 3
+//        for (out = nElems - 1; out >= 1; out--)   // outer loop (backward)
+//            for (in = 0; in < out; in++)        // inner loop (forward)
+//                if (a[in] > a[in + 1])       // out of order?
+//                    swap(in, in + 1);          // swap them
+//    }  // end bubbleSort()
+
+
+    //--------- Project 3.1
+
+    public void bubbleSort() {
+        int out_left, out_right, in;
+
+        // out_left need to be >=1 otherwise the 1st and 2nd elements may not be sorted if number of elements is 3
+        for (out_left = nElems - 1; out_left >= 1; out_left--) {  // outer loop (backward)
+            for (in = 0; in < out_left; in++) {    // inner loop (forward)
+
+                if (a[in] > a[in + 1]) {      // out_left of order?
+                    swap(in, in + 1);
+                }       // swap them
+                display();
+            }
+            for (out_right = out_left - 1; out_right > 0; out_right--) {
+                if (a[out_right] < a[out_right - 1]) {
+                    swap(out_right, out_right - 1);
+                }
+                display();
+
+            }
+        }
+
+    }  // end bubbleSort()
+
+    //--------------------------------------------------------------
+    private void swap(int one, int two) {
+        long temp = a[one];
+        a[one] = a[two];
+        a[two] = temp;
+    }
 //--------------------------------------------------------------
-   private void swap(int one, int two)
-      {
-      long temp = a[one];
-      a[one] = a[two];
-      a[two] = temp;
-      }
-//--------------------------------------------------------------
-   }  // end class ArrayBub
+}  // end class ArrayBub
+
 ////////////////////////////////////////////////////////////////
-class BubbleSortApp
-   {
-   public static void main(String[] args)
-      {
-      int maxSize = 100;            // array size
-      ArrayBub arr;                 // reference to array
-      arr = new ArrayBub(maxSize);  // create the array
+class BubbleSortApp {
+    public static void main(String[] args) {
 
-      arr.insert(77);               // insert 10 items
-      arr.insert(99);
-      arr.insert(44);
-      arr.insert(55);
-      arr.insert(22);
-      arr.insert(88);
-      arr.insert(11);
-      arr.insert(00);
-      arr.insert(66);
-      arr.insert(33);
+        int maxSize = 100;            // array size
+//        int maxSize = 100000;            // array size
 
-      arr.display();                // display items
+        ArrayBub arr;                 // reference to array
+        arr = new ArrayBub(maxSize);  // create the array
 
-      arr.bubbleSort();             // bubble sort them
+        // Experiment
+//        for (int j = 0; j < maxSize; j++) {
+//            long n = (long)(java.lang.Math.random() * (maxSize - 1));
+//            arr.insert(n);
+//        }
+//
+//        for (long j=99999; j>=0 ; j--) {
+//            long n = j;
+//            arr.insert(n);
+//        }
+        arr.insert(99);
+        arr.insert(77);               // insert 10 items
+        arr.insert(55);
+        arr.insert(44);
+        arr.insert(22);
+        arr.insert(88);
+        arr.insert(11);
+        arr.insert(00);
+        arr.insert(66);
+        arr.insert(33);
 
-      arr.display();                // display them again
-      }  // end main()
-   }  // end class BubbleSortApp
+        long startTime = System.nanoTime();
+        arr.display();                // display items
+        arr.bubbleSort();             // bubble sort them
+        arr.display();                // display them again
+        long endTime = System.nanoTime();
+        System.out.println("That took " + (double) (endTime - startTime) / 1_000_000 + " seconds");
+
+    }  // end main()
+}  // end class BubbleSortApp
 ////////////////////////////////////////////////////////////////
--- Chap03/InsertSort/insertSort.java
+++ Chap03/InsertSort/insertSort.java
@@ -2,72 +2,135 @@
 // demonstrates insertion sort
 // to run this program: C>java InsertSortApp
 //--------------------------------------------------------------
-class ArrayIns
-   {
-   private long[] a;                 // ref to array a
-   private int nElems;               // number of data items
-//--------------------------------------------------------------
-   public ArrayIns(int max)          // constructor
-      {
-      a = new long[max];                 // create the array
-      nElems = 0;                        // no items yet
-      }
-//--------------------------------------------------------------
-   public void insert(long value)    // put element into array
-      {
-      a[nElems] = value;             // insert it
-      nElems++;                      // increment size
-      }
-//--------------------------------------------------------------
-   public void display()             // displays array contents
-      {
-      for(int j=0; j<nElems; j++)       // for each element,
-         System.out.print(a[j] + " ");  // display it
-      System.out.println("");
-      }
-//--------------------------------------------------------------
-   public void insertionSort()
-      {
-      int in, out;
+class ArrayIns {
+    private long[] a;                 // ref to array a
+    private int nElems;               // number of data items
 
-      for(out=1; out<nElems; out++)     // out is dividing line
-         {
-         long temp = a[out];            // remove marked item
-         in = out;                      // start shifts at out
-         while(in>0 && a[in-1] >= temp) // until one is smaller,
+    //--------------------------------------------------------------
+    public ArrayIns(int max)          // constructor
+    {
+        a = new long[max];                 // create the array
+        nElems = 0;                        // no items yet
+    }
+
+    //--------------------------------------------------------------
+    public void insert(long value)    // put element into array
+    {
+        a[nElems] = value;             // insert it
+        nElems++;                      // increment size
+    }
+
+    //--------------------------------------------------------------
+    public void display()             // displays array contents
+    {
+        for (int j = 0; j < nElems; j++)       // for each element,
+            System.out.print(a[j] + " ");  // display it
+        System.out.println("");
+    }
+
+    //--------------------------------------------------------------
+    public void insertionSort() {
+        int in, out;
+
+        for (out = 1; out < nElems; out++)     // out is dividing line
+        {
+            long temp = a[out];            // remove marked item
+            in = out;                      // start shifts at out
+            while (in > 0 && a[in - 1] >= temp) // until one is smaller,
             {
-            a[in] = a[in-1];            // shift item to right
-            --in;                       // go left one position
+                a[in] = a[in - 1];            // shift item to right
+                --in;                       // go left one position
             }
-         a[in] = temp;                  // insert marked item
-         }  // end for
-      }  // end insertionSort()
-//--------------------------------------------------------------
-   }  // end class ArrayIns
+            a[in] = temp;                  // insert marked item
+        }  // end for
+    }  // end insertionSort()
+
+    //--------------------------------------------------------------
+    //--------------Project 3.2-------
+    // Return the median value in the array
+    public long median() {
+        insertionSort();
+
+        return a[nElems / 2];
+
+    }
+
+    //--------------Project 3.3-------
+    public void noDups() {
+
+        for (int i = 0; i < nElems - 1; i++) {
+            int shift = 0;
+
+            int j = i;
+            while (a[j] == a[j + 1]) {
+                shift++;
+                j++;
+            }
+
+            for (;shift>=0;shift--)
+            a[j+1-shift] = a[j+1];
+        }
+
+    }
+
+
+}  // end class ArrayIns
+
 ////////////////////////////////////////////////////////////////
-class InsertSortApp
-   {
-   public static void main(String[] args)
-      {
-      int maxSize = 100;            // array size
-      ArrayIns arr;                 // reference to array
-      arr = new ArrayIns(maxSize);  // create the array
+class InsertSortApp {
+    public static void main(String[] args) {
+//        int maxSize = 10000;            // array size
+        int maxSize = 10;
 
-      arr.insert(77);               // insert 10 items
-      arr.insert(99);
-      arr.insert(44);
-      arr.insert(55);
-      arr.insert(22);
-      arr.insert(88);
-      arr.insert(11);
-      arr.insert(00);
-      arr.insert(66);
-      arr.insert(33);
+        ArrayIns arr;                 // reference to array
+        arr = new ArrayIns(maxSize);  // create the array
 
-      arr.display();                // display items
+        // Experiment
+//        for (int j = 0; j < maxSize; j++) {
+//            long n = (long) (java.lang.Math.random() * (maxSize - 1));
+//            arr.insert(n);
+//        }
 
-      arr.insertionSort();          // insertion-sort them
+//        for (long j=99999; j>=0 ; j--) {
+//            long n = j;
+//            arr.insert(n);
+//
+//        }
 
-      arr.display();                // display them again
-      }  // end main()
-   }  // end class InsertSortApp
+
+        arr.insert(77);               // insert 10 items
+        arr.insert(99);
+        arr.insert(44);
+//        arr.insert(55);
+//        arr.insert(22);
+//        arr.insert(88);
+//        arr.insert(11);
+//        arr.insert(00);
+//        arr.insert(66);
+//        arr.insert(33);
+
+        arr.insert(77);
+        arr.insert(99);
+        arr.insert(44);
+        arr.insert(44);
+
+        long startTime = System.nanoTime();
+
+        arr.display();                // display items
+
+        arr.insertionSort();          // insertion-sort them
+
+        arr.display();                // display them again
+
+        long endTime = System.nanoTime();
+        System.out.println("That took " + (double) (endTime - startTime) / 1_000_000 + " seconds");
+
+        // Project 3.2
+        System.out.println("Median value is " + arr.median());
+
+        arr.noDups();
+        arr.display();                // display them again
+
+
+    }  // end main()
+}  // end class InsertSortApp
