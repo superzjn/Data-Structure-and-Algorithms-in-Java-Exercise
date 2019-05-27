d669259c0ddfaa60db1c9fe7362ed44fcca1e15b
Jianan Zhang <superzjn@gmail.com>
2019-05-27 15:26:30 -0500

    Project 4.2 prep

---
+++ Chap04/Queue/Deque.java
@@ -0,0 +1,54 @@
+public class Deque {
+
+    private int nItems;
+    private int leftFront;
+    private int leftRear;
+    private int rightFront;
+    private int rightRear;
+    private int[] queueArray;
+    private int maxSize;
+
+    public Deque(int s) {
+
+    }
+
+    public void insertLeft() {
+
+    }
+
+    public void insertRight() {
+
+    }
+
+    public void removeLeft() {
+
+    }
+
+    public void removeRight() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public boolean isFull() {
+
+    }
+
+}
+
+class dequeApp {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+
+
+
+
+
