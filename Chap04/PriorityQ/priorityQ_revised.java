16bede940585f2ee56b4adce25e164d914ce1269
Jianan Zhang <jianan@Jianans-MacBook-Pro.local>
2019-05-29 20:51:40 -0500

    Project 4.4 in progress

--- Chap04/PriorityQ/priorityQ_revised.java
+++ Chap04/PriorityQ/priorityQ_revised.java
@@ -18,21 +18,39 @@ public class priorityQ_revised {
     //-------------------------------------------------------------
     public void insert(long item)    // insert item
     {
-
-
+        if (!isFull())
+            queArray[nItems++] = item;
+        else
+            System.out.println("Queue is full");
     }  // end insert()
 
     //-------------------------------------------------------------
     public long remove()             // remove minimum item
     {
+        int index = getminIndex();
         return queArray[--nItems];
     }
 
     //-------------------------------------------------------------
     public long peekMin()            // peek at minimum item
     {
-        return queArray[nItems - 1];
+        return queArray[getminIndex()];
     }
+
+    //-------------------------------------------------------------
+    public int getminIndex() {
+        long min = queArray[0];
+        int index = 0;
+
+        for (int i = 0; i < nItems; i++) {
+            if (queArray[i] < min) {
+                min = queArray[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
 
     //-------------------------------------------------------------
     public boolean isEmpty()         // true if queue is empty
@@ -58,11 +76,15 @@ class priorityQ_revisedApp {
         thePQ.insert(40);
         thePQ.insert(20);
 
+        System.out.println("peek " + thePQ.peekMin());
+
         while (!thePQ.isEmpty()) {
             long item = thePQ.remove();
             System.out.print(item + " ");  // 10, 20, 30, 40, 50
         }  // end while
         System.out.println("");
+
+
     }  // end main()
 //-------------------------------------------------------------
 }  // end class PriorityQApp
