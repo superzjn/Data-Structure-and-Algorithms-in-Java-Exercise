7a7bfd07687e95a7dc382829ac599e912046c693
Jianan Zhang <jianan@Jianans-MacBook-Pro.local>
2019-06-24 20:44:15 -0500

    Project 5.6 in progress

---
+++ Chap05/Projects/Project5_6/SpreadSheet.java
@@ -0,0 +1,30 @@
+package Projects.Project5_6;
+
+public class SpreadSheet {
+
+    Link cell;
+
+    public SpreadSheet() {
+
+    }
+}
+
+class Link {
+
+    int iData;
+    Link right;
+    Link under;
+
+    public Link() {
+        iData = 0;
+
+    }
+
+}
+
+class SpreadSheetApp {
+
+    public static void main(String[] args) {
+
+    }
+}
