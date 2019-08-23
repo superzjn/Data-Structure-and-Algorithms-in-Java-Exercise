9cf7d4e11c5997c3be9ca8bfef05f11f26674511
Jianan Zhang <superzjn@gmail.com>
2019-08-22 20:49:09 -0500

    Project 8.5 in progress

--- Chap08/Projects/Project8_5/Huffman.java
+++ Chap08/Projects/Project8_5/Huffman.java
@@ -6,17 +6,22 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.*;
+import java.util.Stack;
 
 
 class Node {
 
-    char data;
+    String data;
     int freq;
     Node leftChild;
     Node rightChild;
 
-    public Node(char data) {
+    public Node(String data) {
         this.data = data;
+    }
+
+    public void displayNode() {
+        System.out.print(data);
     }
 }
 
@@ -26,6 +31,48 @@ class Tree {
     public Tree(Node node) {
         this.root = node;
     }
+
+    // -------------------------------------------------------------
+    public void displayTree() {
+        Stack globalStack = new Stack();
+        globalStack.push(root);
+        int nBlanks = 32;
+        boolean isRowEmpty = false;
+        System.out.println(
+                "......................................................");
+        while (isRowEmpty == false) {
+            Stack localStack = new Stack();
+            isRowEmpty = true;
+
+            for (int j = 0; j < nBlanks; j++)
+                System.out.print(' ');
+
+            while (globalStack.isEmpty() == false) {
+                Node temp = (Node) globalStack.pop();
+                if (temp != null) {
+                    temp.displayNode();
+                    localStack.push(temp.leftChild);
+                    localStack.push(temp.rightChild);
+
+                    if (temp.leftChild != null ||
+                            temp.rightChild != null)
+                        isRowEmpty = false;
+                } else {
+                    System.out.print("--");
+                    localStack.push(null);
+                    localStack.push(null);
+                }
+                for (int j = 0; j < nBlanks * 2 - 2; j++)
+                    System.out.print(' ');
+            }  // end while globalStack not empty
+            System.out.println();
+            nBlanks /= 2;
+            while (localStack.isEmpty() == false)
+                globalStack.push(localStack.pop());
+        }  // end while isRowEmpty is false
+        System.out.println(
+                "......................................................");
+    }  // end displayTree()
 }
 
 class PriorityQueue {
@@ -57,11 +104,11 @@ class PriorityQueue {
 
             nItems++;
         }
-        showQueue();
+        //   showQueue();
 
     }
 
-    public Tree get() {
+    public Tree deQueue() {
         return queArray[--nItems];
     }
 
@@ -80,23 +127,31 @@ class HuffmanCode {
     public Map generateFreqTable(String input) {
 
         String text = input.trim().toUpperCase();
-        Map<Character, Integer> table = new HashMap<Character, Integer>();
+        Map<String, Integer> table = new HashMap<String, Integer>();
         for (int i = 0; i < text.length(); i++) {
             char ch = text.charAt(i);
-            table.putIfAbsent(ch, 0);
-            if (table.containsKey(ch)) {
-                int count = (int) table.get(ch);
-                table.replace(ch, ++count);
+            StringBuilder builder = new StringBuilder();
+            if (ch == ' ') {
+                builder.append("Sp");
+            } else {
+                builder.append(ch);
+            }
+            String key = builder.toString();
+
+            table.putIfAbsent(key, 0);
+            if (table.containsKey(key)) {
+                int count = (int) table.get(key);
+                table.replace(key, ++count);
             }
         }
         return table;
     }
 
-    public Tree generateTree(Map<Character, Integer> table) {
+    public Tree generateTree(Map<String, Integer> table) {
 
         PriorityQueue queue = new PriorityQueue(table.size());
 
-        for (Entry<Character, Integer> entry : table.entrySet()) {
+        for (Entry<String, Integer> entry : table.entrySet()) {
             Node node = new Node(entry.getKey());
             node.freq = entry.getValue();
             Tree tree = new Tree(node);
@@ -104,15 +159,19 @@ class HuffmanCode {
         }
 
         while (queue.nItems > 1) {
-            Tree tree1 = queue.get();
-            Tree tree2 = queue.get();
+            Tree tree1 = queue.deQueue();
+            Tree tree2 = queue.deQueue();
+            Node leftnode = tree1.root;
+            Node rightnode = tree2.root;
 
-            Node newnode = new Node(' ');
-            newnode.leftChild = tree1.root;
-            newnode.rightChild = tree2.root;
-            // TODO p 419 step2
+            Node newnode = new Node("ep");
+            newnode.freq = leftnode.freq + rightnode.freq;
+            newnode.leftChild = leftnode;
+            newnode.rightChild = rightnode;
+            Tree newtree = new Tree(newnode);
+            queue.add(newtree);
         }
-        return null;
+        return queue.deQueue();
     }
 }
 
@@ -123,6 +182,7 @@ class App {
         String input = reader.readLine();
         HuffmanCode tree = new HuffmanCode();
         Map map = tree.generateFreqTable(input);
-        tree.generateTree(map);
+        Tree huffmantree = tree.generateTree(map);
+        huffmantree.displayTree();
     }
 }
